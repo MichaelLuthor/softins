2dc71356822acdc73468e23af8e1f35812abd323
root <root@localhost.localdomain>
2017-12-11 17:53:00 +0800

    redmine

--- redmine.sh
+++ redmine.sh
@@ -89,8 +89,11 @@ cd $install_path
 
 
 # check ruby or install
+ruby_path=""
 command -v ruby
 if [ $? -ne 0 ]; then
+  ruby_path="$install_path/ruby/bin"
+
   cd install
   if [ ! -f "ruby.tar.gz" ]; then
     wget https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.2.tar.gz -O ruby.tar.gz
@@ -149,7 +152,7 @@ cd ../
 
 
 # setup
-#cd redmine
+cd redmine
 #echo "production:" >> config/database.yml
 #echo "  adapter: mysql2" >> config/database.yml
 #echo "  database: $mysql_dbname" >> config/database.yml
@@ -169,55 +172,21 @@ cd ../
 #RAILS_ENV=production bundle exec rake db:migrate
 #RAILS_ENV=production bundle exec rake redmine:load_default_data
 
-mkdir -p tmp tmp/pdf public/plugin_assets
-chown -R redmine:redmine files log tmp public/plugin_assets
-chmod -R 755 files log tmp public/plugin_assets
+#mkdir -p tmp tmp/pdf public/plugin_assets
+#chown -R redmine:redmine files log tmp public/plugin_assets
+#chmod -R 755 files log tmp public/plugin_assets
 
-# start
-bundle exec rails server webrick -e production
-
-
-
-
-
-
-
-
-
-
-
-
-# check ruby or install
-command -v ruby
-if [ $? -ne 0 ]; then
-  echo "install ruby 2.4.2"
-  if [ ! -f "ruby.tar.gz" ]; then
-    wget https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.2.tar.gz -O ruby.tar.gz
-  fi
-  if [ ! -d "ruby" ]; then
-    tar -xf ruby.tar.gz
-    mv ruby-2.4.2 ruby-src
-    cd ruby-src
-    ./configure   \
-      --prefix=$install_path/ruby \
-      --oldincludedir=$install_path/ruby/include \
-      --datadir=$install_path/ruby/data \
-      --htmldir=$install_path/ruby/doc/html \
-      --dvidir=$install_path/ruby/doc/dvi \
-      --pdfdir=$install_path/ruby/doc/pdf \
-      --psdir=$install_path/ruby/doc/ps \
-      --disable-install-doc 
-    make
-    make install
-  fi
-  
-  export PATH=$PATH:$install_path/ruby/bin/
+# create tools
+echo "#/bin/bash" >> test.sh
+if [ $ruby_path ]; then
+  echo "old_env_path=\$PATH"
+  echo "export PATH=\$PATH:$ruby_path" >> test.sh
 fi
-
-
-
-
-exit
+echo "bundle exec rails server webrick -e production" >> test.sh
+if [ $ruby_path ]; then
+  echo "export PATH=\$old_env_path" >> test.sh
+fi
+chmod u+x test.sh
 
 # Resotre old env value
 export PATH=$old_env_path
